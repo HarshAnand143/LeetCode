19e7fdd3beb6cfc2ca980493aef344de6b992493
Harsh Anand <harshanandsah@gmail.com>
2026-09-20 22:16:09 +0530

    Time: 2144 ms (5.05%) | Memory: 7.7 MB (90.76%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int ways(int n) {
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            sum += ways(i) * ways(n - 1 - i);
+        }
+        return sum;
+    }
+    int numTrees(int n) {
+        return ways(n);
+    }
+};
