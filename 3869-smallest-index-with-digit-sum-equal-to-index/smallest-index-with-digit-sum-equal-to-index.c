b09f5bc770308c528a51aae565f0b153378024a1
Harsh Anand <harshanandsah@gmail.com>
2026-09-24 20:07:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (3.85%) - LeetSync

---
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.c
@@ -0,0 +1,14 @@
+int smallestIndex(int* nums, int numsSize) {
+    for (int i = 0; i < numsSize; i++) {
+            int x = nums[i];
+            int sum = 0;
+            while (x > 0) {
+                sum += x % 10;
+                x /= 10;
+            }
+            if (sum == i) {
+                return i;
+            }
+        }
+        return -1;
+}
